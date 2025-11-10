427364feb4a196014460912410e202886452d566
Hamid Haghdoost <tuytoosh@yahoo.com>
2025-11-10 22:25:11 +0100

    S3 - Abstract Classes

--- index.php
+++ index.php
@@ -21,8 +21,33 @@ require "./Product.php";
 // Is a 
 // Eproduct is a Product 
 
-$myebook = new EProduct("My Ebook");
-$myebook->setLink("http;s;//sdfdjf");
+// $myebook = new EProduct("My Ebook");
+// $myebook->setLink("http;s;//sdfdjf");
 
-$mobile = new PhysicalProduct("Appple iphone 15");
-$mobile->setWeight(120);
+// $mobile = new PhysicalProduct("Appple iphone 15");
+// $mobile->setWeight(120);
+
+abstract class Vehicle {
+    public function test() {
+        return "test";
+    }
+
+    abstract function drive();
+}
+
+class Car extends Vehicle {
+    public function drive() {
+        echo "Driving...";
+    }
+}
+
+class Bus extends Vehicle {
+    public function drive() {
+        
+    }
+}
+
+$t = new Car();
+var_dump($t->test());
+
+$t->drive();
